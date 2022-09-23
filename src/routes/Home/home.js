5231eed7404195a8af7f396569b58f4e7252135b
Saqib <saqibshah2882@gmail.com>
2022-09-23 17:14:49 +0500

    Portfolio template ready

--- src/routes/Home/home.js
+++ src/routes/Home/home.js
@@ -11,6 +11,28 @@ const Home = ({theme}) => {
                 <div className="name">
                 <span className={`f-${theme}`}>Saqib Ali</span> Shah
                 </div>
+                <div class="social-menu">
+                <a href="https://github.com/Saqib-002">
+                    <div className={`social-menu-item f-${theme}`} social-menu-name="Github">
+                        <img  src="/assets/socials/github.svg" alt=""/>
+                    </div>
+                </a>
+                <a href="https://www.facebook.com/syedsaqib.alishah.75">
+                    <div className={`social-menu-item f-${theme}`} social-menu-name="Facebook">
+                        <img  src="/assets/socials/facebook.svg" alt=""/>
+                    </div>
+                </a>
+                <a href="https://twitter.com/saqibshah17201">
+                    <div className={`social-menu-item f-${theme}`} social-menu-name="Twitter">
+                        <img  src="/assets/socials/twitter.svg" alt=""/>
+                    </div>
+                </a>
+                <a href="https://www.linkedin.com/in/saqib-ali-shah-b991941b8/">
+                    <div className={`social-menu-item f-${theme}`} social-menu-name="LinkedIn">
+                        <img  src="/assets/socials/linkedin.svg" alt=""/>
+                    </div>
+                </a>
+                </div>
                 <div className={`field-container f-${theme}`}>
                     <span className="pre-field">I am a </span>
                     <Typewriter
